cacbf2d833b0d423b07b8915914349d21261511c
Clément Ababou <clement.ababou@outlook.com>
2022-03-23 00:30:45 +0100

    Base commit of autogenerated spring boot project

---
+++ src/main/java/fr/klemms/ttvrightnow/ttvrightnow/TtvrightnowApplication.java
@@ -0,0 +1,13 @@
+package fr.klemms.ttvrightnow.ttvrightnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtvrightnowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtvrightnowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/klemms/ttvrightnow/ttvrightnow/TtvrightnowApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.klemms.ttvrightnow.ttvrightnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtvrightnowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
